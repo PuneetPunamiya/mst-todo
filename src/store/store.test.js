f47b97aca1b10892186ae054bb1026ef182f14b2
vinamra28 <vinjain@redhat.com>
2021-10-05 14:37:16 +0530

    Add snapshot tests for store

--- src/store/store.test.js
+++ src/store/store.test.js
@@ -1,13 +1,22 @@
-import {RootStore} from './store'
-import {getSnapshot} from 'mobx-state-tree'
+import { RootStore } from "./store";
+import { getSnapshot } from "mobx-state-tree";
 
 it("creates a store", () => {
+  const store = RootStore.create({});
+  store.addTodo(1, "Eat a cake");
 
-  const store = RootStore.create({})
-  store.addTodo(1, "Eat a cake")
+  store.addTodo(1, "Eat a cake");
+  expect(store.todos.size).toBe(1);
+  store.todos.get(1).toggle();
+  expect(store.todos.get(1).done).toBe(true);
+});
 
-  store.addTodo(1, "Eat a cake")
-  expect(store.todos.size).toBe(1)
-  store.todos.get(1).toggle()
-  expect(store.todos.get(1).done).toBe(true)
-})
+it("creates a store to match snapshot", () => {
+  const store = RootStore.create({});
+  store.addTodo(1, "Eat a cake");
+
+  store.addTodo(1, "Eat a cake");
+  expect(store.todos.size).toBe(1);
+  store.todos.get(1).toggle();
+  expect(store).toMatchSnapshot();
+});
