a20a9f8a7ee7df5ad47603305a8e49592863efd7
vinamra28 <vinjain@redhat.com>
2021-10-05 14:39:30 +0530

    Add tests for model

--- src/store/model.test.js
+++ src/store/model.test.js
@@ -10,3 +10,12 @@ it("shoult toggle", () => {
   s.todos[0].toggle()
   expect(s.todos[0].done).toBe(true)
 })
+
+it("should test with snapshots", () => {
+  const s = TodoStore.create({ todos: [] })
+  unprotect(s)
+  s.todos.push({ task: "asd" })
+  // expect(s.todos[0].done).toBe(false)
+  s.todos[0].toggle()
+  expect(s).toMatchSnapshot();
+})
